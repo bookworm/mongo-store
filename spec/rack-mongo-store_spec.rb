b8e90532a4e3fe0ab45ce8bef677f2d34d7800d1
Pete Brumm <pbrumm@edgenet.com>
2011-03-23 13:30:10 -0500

    fixing spec for latest mongo libraries

--- spec/rack-mongo-store_spec.rb
+++ spec/rack-mongo-store_spec.rb
@@ -30,8 +30,8 @@ describe 'Rack::Session::Mongo' do
       :collection => 'mongo-test')
     pool = mongo.pool
     connection = mongo.connection
-    
-    connection.host.should == 'localhost'
+
+    connection.primary[0].should == 'localhost'
     
     pool.should be_kind_of(Mongo::Collection)
     pool.db.name.should == 'rack-test'
